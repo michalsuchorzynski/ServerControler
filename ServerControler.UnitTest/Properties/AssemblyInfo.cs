4e078a5457bdf73892551c6ac3ecf5ad1c14e1a9
michalsuchorzynski <michal.suchorzynski@gmail.com>
2017-07-01 13:46:17 +0200

    Add ServerControler.AdminApp project to solution Add ServerControler.ClientWeb project to solution Add ServerControler.EntityModel project to solution Add ServerControler.TestConsole project to solution Add ServerControler.UnitTest project to solution

---
+++ ServerControler.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ServerControler.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServerControler.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84f3cfbe-0f8b-4120-aeb8-9925b55b7c4a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
